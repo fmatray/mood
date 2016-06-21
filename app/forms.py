000176c1e9daa71b8c96d2b16cf9eaddea5e4eb0
Frédéric <fmatray@users.noreply.github.com>
2016-06-21 15:13:22 +0200

    Cleaning

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import Form
-from wtforms import BooleanField, StringField, PasswordField, SubmitField, validators
+from wtforms import StringField, SubmitField, validators
 
 
 class TeamInvite(Form):
-	email=StringField('Email to invite', [validators.Length(min=6, max=35), validators.Email()])
+	email=StringField('Email to invite', [
+                          validators.Length(min=6, max=35), validators.Email()])
 	submit=SubmitField('Go')
 	
 	def __init__(self):
