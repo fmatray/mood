0796575feccf14b7aac677b7db2b2516cec25f53
Frédéric <fmatray@users.noreply.github.com>
2016-06-21 15:20:09 +0200

    add docstring

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, SubmitField, validators
 
 
 class TeamInvite(Form):
+	""" Form to send and invitation for a team to a user """
 	email=StringField('Email to invite', [
                           validators.Length(min=6, max=35), validators.Email()])
 	submit=SubmitField('Go')
